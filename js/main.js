f139b67321e0bec6e9c504d9e7a766271f623349
Aleksey <alexey-step@mail.ru>
2020-10-11 17:59:59 +0500

    Выполнил задание часть 2

--- js/main.js
+++ js/main.js
@@ -8,6 +8,54 @@
   window.elements.addressInput.setAttribute(`readonly`, ``);
   window.elements.form.addEventListener(`change`, window.form.formHandler);
 
-  window.elements.mapPinMain.addEventListener(`mousedown`, window.map.onMouseDownPress);
   window.elements.mapPinMain.addEventListener(`keydown`, window.map.onEnterPress);
+
+  window.elements.mapPinMain.addEventListener(`mousedown`, (evt) => {
+    evt.preventDefault();
+
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    let drug = false;
+
+    const onMouseMove = (moveEvt) => {
+      moveEvt.preventDefault();
+
+      drug = true;
+
+      let shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((window.elements.mapPinMain.offsetTop - shift.y) > window.util.PIN_COORDS.MIN_Y && (window.elements.mapPinMain.offsetTop - shift.y) < window.util.PIN_COORDS.MAX_Y) {
+        window.elements.mapPinMain.style.top = `${window.elements.mapPinMain.offsetTop - shift.y}px`;
+      }
+      if ((window.elements.mapPinMain.offsetLeft - shift.x) < window.util.PIN_COORDS.MAX_X && (window.elements.mapPinMain.offsetLeft - shift.x) > window.util.PIN_COORDS.MIN_X) {
+        window.elements.mapPinMain.style.left = `${window.elements.mapPinMain.offsetLeft - shift.x}px`;
+      }
+    };
+
+    const onMouseUp = (upEvt) => {
+      upEvt.preventDefault();
+
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+      window.elements.addressInput.value = window.form.getMainPinCoordinates();
+
+      if (!drug) {
+        window.map.activateMap();
+      }
+    };
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
+  });
 })();
+
